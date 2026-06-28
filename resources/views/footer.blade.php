caab4d0f818a2e12e31f76c82fb034a04e8f0b0a
Mo <mo.ghorb@gmail.com>
2026-06-28 12:01:46 +0300

    style(footer): enhance footer text styling for better readability

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
-<div id="footer" class="text-center d-flex align-items-center" style="background-color: #2d3436;color:white;" >
+<div id="footer" class="text-center d-flex align-items-center" style="background-color: #2d3436;color:white;font-size:0.875rem;" >
     <div class="mx-auto my-auto" style="padding:5px;">Powered by 
-        <span class="lead" style="color:#74b9ff;">Laravel</span>,
-        <span class="lead" style="color:#ff7675;">Bootstrap</span>
-         and <span class="lead" style="color:#00b894;">Telegram</span>
+        <span style="color:#74b9ff;font-size:1.05rem;font-weight:300;">Laravel</span>,
+        <span style="color:#ff7675;font-size:1.05rem;font-weight:300;">Bootstrap</span>
+         and <span style="color:#00b894;font-size:1.05rem;font-weight:300;">Telegram</span>
     </div>
 </div>
